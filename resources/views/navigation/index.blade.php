a973f83ac9250a203cd92910aa48914eafe4c7cd
Darko Pokrajac <darko.pokrajac@gmail.com>
2023-07-26 21:25:22 +0000

    ADDED Create links to Users, Roles, Pages & Navigations groups in Dashboard

--- resources/views/navigation/index.blade.php
+++ resources/views/navigation/index.blade.php
@@ -3,6 +3,9 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
         Registrated Vavigations' List
         </h2>
+        <div>
+        <a href="{{ route('navigation.create') }}" class="ml-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Create New</a>
+        </div>
     </x-slot>
 
     <div class="container mx-auto flex justify-center">
